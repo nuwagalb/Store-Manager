afde95f9d43c8e2f583530e1d877d47f0e20946f
nuwagalb <isnuwa@gmail.com>
2018-10-18 12:14:50 +0300

    ft(api home page) Admins and store attendants should be able to view and access their authorized endpoints This commit delivers the creation of the api home page [Delivers #161310221]

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 api = Flask(__name__)
 
 @api.route("/")
 def index():
     """returns the index.html template"""
-    return render_template('index.html')
+    role = request.args.get('role')
 
+    if not role:
+        role = "Visitor"
+
+    return render_template('index.html', role=role)
 if __name__ == "__main__":
-    api.run(debug=True)   
+    api.run(debug=True)
