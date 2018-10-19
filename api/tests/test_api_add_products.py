3dd96b39a64729578bca87b80228f5bf3ecf071e
nuwagalb <isnuwa@gmail.com>
2018-10-19 04:24:31 +0300

    ft(add a produt) The Admin should be able to add a new product This commit delivers the creation of the add product api endpoint [Delivers #161267559]

---
+++ api/tests/test_api_add_products.py
@@ -0,0 +1,135 @@
+import unittest
+from flask import json, jsonify, make_response
+from app import api
+from ..resources.products.products import Product
+
+class AddProductTestCase(unittest.TestCase):
+    """
+        contains tests for the ADD PRODUCT API ENDPOINT
+    """
+    def setUp(self):
+        """sets up a new wsgi instance and a new post request to add a product"""
+        self.api = api.test_client()
+        self.api.post('/api/v1/products', content_type="application/json",
+                      data=json.dumps({"name": "Nokia 3", "price": 341.62, "quantity": 6.00})
+                 )
+
+    def test_for_missing_name_key(self):
+        """tests for missing name key"""
+        response = self.api.post('/api/v1/products', content_type='application/json',
+                                data=json.dumps({"": "Nokia 3", "price": 341.62, "quantity": 6.00}))
+
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'Request is missing the product name key')
+
+    def test_for_missing_price_key(self):
+        """tests for missing price key"""
+        response = self.api.post('/api/v1/products', content_type='application/json',
+                                 data=json.dumps({"name": "Nokia 3", "": 341.62, "quantity": 6.00}))
+
+        data = json.loads(response.data)                   
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'Request is missing the product price key')
+
+    def test_for_missing_quantity_key(self):
+        """tests for missing quantity key"""
+        response = self.api.post('/api/v1/products', content_type='application/json',
+                                 data=json.dumps({"name": "Nokia 3", "price": 341.62, "": 6.00}))
+
+        data = json.loads(response.data)                   
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'Request is missing the product quantity key')
+
+    def test_for_extra_keys_passed_to_the_request(self):
+        """tests for extra keys passed in request"""
+        response = self.api.post('/api/v1/products', content_type='application/json',
+                                 data=json.dumps({"name": "Nokia 3", "price": 341.62,
+                                                  "quantity": 6.00, "password": 'sql'})
+                    )
+
+        data = json.loads(response.data)                   
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'Request has more keys than expected')
+
+    def test_for_invalid_type_of_data_passed_to_the_name_key(self):
+        """tests for a non string passed as value for name key request"""
+        response = self.api.post('/api/v1/products', content_type='application/json',
+                                 data=json.dumps({"name": 5673, "price": 341.62, "quantity": 6.00}))
+
+        data = json.loads(response.data)                   
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'Invalid data type for name value. Please enter a string')
+
+    def test_for_empty_name_details(self):
+        """tests for empty name details"""
+        response = self.api.post('/api/v1/products', content_type='application/json',
+                                 data=json.dumps({"name": '', "price": 341.62, "quantity": 6.00}))
+
+        data = json.loads(response.data)                   
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'The name of the product cannot be empty')
+
+    def test_for_invalid_type_of_data_passed_to_the_price_key(self):
+        """tests for a string passed as value for price key request"""
+        response = self.api.post('/api/v1/products', content_type='application/json',
+                                 data=json.dumps({"name": 'Nokia 3', "price": '341.62', "quantity": 6.00}))
+
+        data = json.loads(response.data)                   
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'Invalid data type for price value. Please enter a float')
+
+    def test_for_empty_price_details(self):
+        """tests for empty price details"""
+        response = self.api.post('/api/v1/products', content_type='application/json',
+                                 data=json.dumps({"name": 'Nokia 3', "price": 0.00, "quantity": 6.00}))
+
+        data = json.loads(response.data)                   
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'The price of the product cannot be empty')
+
+    def test_for_price_value_being_greater_than_zero(self):
+        """tests for negative price details"""
+        response = self.api.post('/api/v1/products', content_type='application/json',
+                                 data=json.dumps({"name": 'Nokia 3', "price": -5.43, "quantity": 6.00}))
+
+        data = json.loads(response.data)                   
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'The price of the product cannot be zero or less than zero')
+
+    def test_for_invalid_type_of_data_passed_to_the_quantity_key(self):
+        """tests for a string passed as value for quantity key request"""
+        response = self.api.post('/api/v1/products', content_type='application/json',
+                                 data=json.dumps({"name": 'Nokia 3', "price": 341.62, "quantity": '6.00'}))
+
+        data = json.loads(response.data)                   
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'Invalid data type for quantity value. Please enter a float')
+
+    def test_for_empty_quantity_details(self):
+        """tests for empty quantity details"""
+        response = self.api.post('/api/v1/products', content_type='application/json',
+                                 data=json.dumps({"name": 'Nokia 3', "price": 341.62, "quantity": 0.00}))
+
+        data = json.loads(response.data)                   
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'The quantity of the product cannot be empty')
+
+    def test_for_quantity_value_being_greater_than_zero(self):
+        """tests for negative quantity details"""
+        response = self.api.post('/api/v1/products', content_type='application/json',
+                                 data=json.dumps({"name": 'Nokia 3', "price": 341.62, "quantity": -6.00}))
+
+        data = json.loads(response.data)                   
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'The quantity of the product cannot be zero or less than zero')
+
+    def test_for_successful_addition_of_a_product(self):
+        """tests for successfully adding a product"""
+        response = self.api.post('/api/v1/products', content_type='application/json',
+                                 data=json.dumps({"name": 'Nokia 3', "price": 341.62, "quantity": 6.00}))
+
+        #data = json.loads(response.data)                   
+        self.assertEqual(response.status_code, 201)
+        #self.assertEqual(data['message'], 'The product was successfully added')
+        
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json, jsonify
+from api.resources.products.products import Product
 
 api = Flask(__name__)
 
@@ -11,5 +12,91 @@ def index():
         role = "Visitor"
 
     return render_template('index.html', role=role)
+
+#PRODUCTS
+#add new product
+@api.route("/api/v1/products", methods=['POST'])
+def add_product():
+    """adds new product"""
+    json_data = request.get_json()
+
+    if 'name' not in json_data:
+        response_object = {'message': 'Request is missing the product name key'}
+        return jsonify(response_object), 400
+
+    if 'price' not in json_data:
+        response_object = {'message': 'Request is missing the product price key'}
+        return jsonify(response_object), 400
+
+    if 'quantity' not in json_data:
+        response_object = {'message': 'Request is missing the product quantity key'}
+        return jsonify(response_object), 400
+
+    if len(json_data.keys()) > 3:
+        response_object = {'message': 'Request has more keys than expected'}
+        return jsonify(response_object), 400
+
+    if not isinstance(json_data.get('name'), str):
+        response_object = {'message': 'Invalid data type for name value. Please enter a string'}
+        return jsonify(response_object), 400
+
+    if json_data.get('name') == '':
+        response_object = {'message': 'The name of the product cannot be empty'}
+        return jsonify(response_object), 400
+
+    if not isinstance(json_data.get('price'), float):
+        response_object = {'message': 'Invalid data type for price value. Please enter a float'}
+        return jsonify(response_object), 400
+
+    if json_data.get('price') == 0.00:
+        response_object = {'message': 'The price of the product cannot be empty'}
+        return jsonify(response_object), 400
+
+    if not isinstance(json_data.get('quantity'), float):
+        response_object = {'message': 'Invalid data type for quantity value. Please enter a float'}
+        return jsonify(response_object), 400
+
+    if json_data.get('quantity') == 0.00:
+        response_object = {'message': 'The quantity of the product cannot be empty'}
+        return jsonify(response_object), 400
+
+    if json_data.get('price') <= 0.00:
+        response_object = {'message':  'The price of the product cannot be zero or less than zero'}
+        return jsonify(response_object), 400
+
+    if json_data.get('quantity') <= 0.00:
+        response_object = {'message':  'The quantity of the product cannot be zero or less than zero'}
+        return jsonify(response_object), 400
+
+    new_product = Product(json_data['name'], json_data['price'], json_data['quantity'])
+    product_status = new_product.add_product()
+
+    if product_status is True:
+        response_object = {'message':  'The product was successfully added'}
+    return jsonify(response_object), 201
+
+
+    
+        
+@api.errorhandler(404)
+def page_not_found(error):
+    """displays page when 404 error code is raised"""
+    return render_template('404.html'), 404
+
+@api.errorhandler(401)
+def unauthorized_access(error):
+    """displays page when 401 error code is raised"""
+    return render_template('401.html'), 401
+
+@api.errorhandler(500)
+def internal_server_error(error):
+    """displays page when 500 error code is raised"""
+    return render_template('500.html'), 500
+
+@api.errorhandler(405)
+def wrong_method(error):
+    """displays page when 405 error code is raised"""
+    return render_template('405.html'), 405
+
 if __name__ == "__main__":
     api.run(debug=True)
