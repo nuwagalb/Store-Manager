9ac7096ed437a57be917eafae66f2940de625088
nuwagalb <isnuwa@gmail.com>
2018-10-24 23:20:55 +0300

    modify files to improve on user interface

---
+++ UI/js/main.js
@@ -0,0 +1,15 @@
+function DisplayViewSingleProductPage() {
+    window.location.href = "view_single_product.html";
+}
+
+function DisplayEditProductPage() {
+    window.location.href = "edit_product.html";
+}
+
+function DisplayAddProductPage() {
+    window.location.href = "add_product.html";
+}
+
+function DisplayViewSingleSalePage() {
+    window.location.href = "view_single_sale.html";
+}
