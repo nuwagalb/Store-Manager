a9b91ae608af3c3026c003cff6254f76f86a24cf
nuwagalb <isnuwa@gmail.com>
2018-10-26 02:23:15 +0300

    modify files to add page responsiveness

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,3 +1,13 @@
+function DisplayLoginResults() {
+    if(document.getElementById("login-role").value == "admin") {
+        window.location.assign("view_all_products.html");
+        return false;
+    } else if (document.getElementById("login-role").value == "attendant"){
+        window.location.assign("shopping-cart.html");
+        return false;
+    }
+}
+
 function DisplayViewSingleProductPage() {
     window.location.href = "view_single_product.html";
 }
@@ -12,4 +22,12 @@ function DisplayAddProductPage() {
 
 function DisplayViewSingleSalePage() {
     window.location.href = "view_single_sale.html";
+}
+
+function DisplayViewSingleAttendantProductPage() {
+    window.location.href = "attendant_single_product.html";
+}
+
+function DisplayViewSingleAttendantSalePage() {
+    window.location.href = "attendant_single_sale.html";
 }
